a448ca62ebd7ab81d4cf7787b4f416c4f051fa24
zinglax <dylanzingler@gmail.com>
2015-04-01 00:26:28 -0400

    added programs

---
+++ Programs/upc_lookup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#
+#
+
+import sys
+from xmlrpclib import ServerProxy, Error
+
+rpc_key = '267b49217902750ecbe58a0e122b7394ab81199b'
+
+if __name__=='__main__':
+	if len(sys.argv) != 2:
+		print 'Usage: fetchupc.py <upc>'
+		exit
+	else:
+		s = ServerProxy('http://www.upcdatabase.com/xmlrpc')
+		params = { 'rpc_key': rpc_key, 'upc': sys.argv[1] }
+		print s.lookup(params)
